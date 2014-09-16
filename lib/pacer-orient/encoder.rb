e6e4f466fc45dd143b404959a228c5a12dae2ec9
Darrick Wiebe <dw@xnlogic.com>
2014-09-16 16:44:11 -0400

    Use binary encoding for dates

--- lib/pacer-orient/encoder.rb
+++ lib/pacer-orient/encoder.rb
@@ -23,10 +23,20 @@ module Pacer::Orient
         end
       when true, false
         value.to_java
-      when JavaDate, Time
-        value
-      when DateTime, Date
-        value.to_time
+      when DateTime, Time, JavaDate
+        f = value.to_time.to_f
+        i = f.truncate
+        r = (f.remainder(1) * 10000).round
+        if value.is_a? DateTime
+          c = 1
+        elsif value.utc?
+          c = 2
+        else
+          c = 3
+        end
+        ["D", i, r, c].pack("ANnc").to_java_bytes
+      when Date
+        ["D", value.to_time.to_i].pack("AN").to_java_bytes
       when Set
         value.to_hashset
       when Hash
@@ -41,12 +51,34 @@ module Pacer::Orient
     end
 
     def self.decode_property(value)
-      puts value.class
       case value
       when ArrayJavaProxy
-        Marshal.load String.from_java_bytes(value)
+        str = String.from_java_bytes(value)
+        flag = str[0]
+        case flag
+        when 'D'
+          data = str.unpack("ANnc")
+          i = data[1]
+          r = data[2]
+          c = data[3]
+          if r
+            t = Time.at(i + (r / 10000.0))
+            case c
+            when 1
+              t.utc.to_datetime
+            when 2
+              t.utc
+            else
+              t
+            end
+          else
+            Time.at(i).utc.to_date
+          end
+        else
+          Marshal.load str
+        end
       when JavaDate
-        Time.at(value.getTime() / 1000.0).utc
+        Time.at(value.getTime() / 1000.0)
       when JavaSet
         value.to_set
       when JavaMap
