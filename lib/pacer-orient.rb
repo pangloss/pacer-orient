312e0c48dc08fb7e7e8d43b5c0ccd2b16bbd4655
Darrick Wiebe <dw@xnlogic.com>
2014-09-20 13:51:30 -0400

    Make the graph factory awesome

--- lib/pacer-orient.rb
+++ lib/pacer-orient.rb
@@ -14,6 +14,30 @@ Pacer::FunctionResolver.clear_cache
 module Pacer
   # Add 'static methods' to the Pacer namespace.
   class << self
+    def orient_factory(url = nil, args = {})
+      if url.nil?
+        url = "memory:#{ next_orient_name }"
+      elsif url.is_a? String and url !~ /^(plocal|local|remote|memory):/
+        url = "plocal:#{ url }"
+      end
+      if url
+        factory = Pacer.open_graphs[url]
+        if factory
+          factory
+        else
+          username = args[:username]
+          password = args[:password]
+          factory =
+            if username
+              com.tinkerpop.blueprints.impls.orient.OrientGraphFactory.new url, username, password
+            else
+              com.tinkerpop.blueprints.impls.orient.OrientGraphFactory.new url
+            end
+          Pacer::Orient::FactoryContainer.new(factory, url, args)
+        end
+      end
+    end
+
     # Return a graph for the given path. Will create a graph if none exists at
     # that location.
     #
@@ -21,52 +45,18 @@ module Pacer
     # Ruby's at_exit callback, but if an already open graph is given, it will
     # not.
     def orient(url = nil, args = nil)
-      if url.nil?
-        url = "memory:#{ next_orient_name }"
-      elsif url.is_a? String and url !~ /^(plocal|local|remote|memory):/
-        url = "plocal:#{ url }"
-      end
-      if args
-        username          = args.delete :username
-        password          = args.delete :password
-        transactional     = args.delete :transactional
-        lightweight_edges = args.delete :lightweight_edges
-        edge_classes      = args.delete :edge_classes
-        vertex_classes    = args.delete :vertex_classes
-      end
-      if url.is_a? String
+      if url.is_a? Pacer::Graph
+        # Don't register the new graph so that it won't be automatically closed.
+        Orient::Graph.new Pacer::Orient::Encoder, proc { url }
+      else
         open = proc do
-          # TODO: can / should I cache connections? Is it essential to shut down Orient?
-          factory = Pacer.open_graphs[[url, username]]
-          unless factory
-            factory =
-              if username
-                com.tinkerpop.blueprints.impls.orient.OrientGraphFactory.new url, username, password
-              else
-                com.tinkerpop.blueprints.impls.orient.OrientGraphFactory.new url
-              end
-            Pacer.open_graphs[[url, username]] = Pacer::Orient::FactoryContainer.new(factory)
-          end
-          if transactional == false
-            graph = factory.getNoTx()
-          else
-            graph = factory.getTx()
-          end
-          graph.useLightweightEdges = lightweight_edges if lightweight_edges == false
-          graph.useClassForEdgeLabel = edge_classes if edge_classes == false
-          graph.useClassForVertexLabel = vertex_classes if vertex_classes == false
-          #graph.setAutoStartTx false
-          graph
+          orient_factory(url, args).get
         end
         shutdown = proc do |g|
-          factory = Pacer.open_graphs.delete [url, username]
+          factory = Pacer.open_graphs.delete url
           factory.shutdown if factory
         end
         Orient::Graph.new(Pacer::Orient::Encoder, open, shutdown)
-      else
-        # Don't register the new graph so that it won't be automatically closed.
-        graph = url
-        Orient::Graph.new Pacer::Orient::Encoder, proc { graph }
       end
     end
 
--- lib/pacer-orient/factory_container.rb
+++ lib/pacer-orient/factory_container.rb
@@ -1,26 +1,71 @@
 module Pacer::Orient
   class FactoryContainer
-    attr_reader :factory
+    attr_reader :factory, :url
+    attr_accessor :encoder, :transactional, :lightweight_edges, :edge_classes, :vertex_classes
 
-    def initialize(f)
+    def initialize(f, url, args)
       @factory = f
+      @url = url
+      Pacer.open_graphs[url] = self
+      if args
+        @transactional     = args[:transactional]
+        @lightweight_edges = args[:lightweight_edges]
+        @edge_classes      = args[:edge_classes]
+        @vertex_classes    = args[:vertex_classes]
+        self.stay_open     = args[:stay_open]
+        @encoder = args.fetch :encoder, Encoder
+        min = args[:pool_min]
+        max = args[:pool_max]
+        setupPool min, max if min and max
+      end
+    end
+
+    def setupPool(min, max)
+      factory.setupPool min, max
+    end
+
+    def stay_open=(b)
+      #factory.setLeaveGraphsOpen b
+    end
+
+    def stay_open
+      #factory.getLeaveGraphsOpen
+    end
+
+    def graph
+      Graph.new encoder, proc { get }
     end
 
     def get
-      factory.get
+      if transactional == false
+        getNoTx
+      else
+        getTx
+      end
     end
 
     def getTx
-      factory.getTx
+      configure factory.getTx
     end
 
     def getNoTx
-      factory.getNoTx
+      configure factory.getNoTx
     end
 
     # Pacer calls shutdown on all cached graphs when it exits. Orient caches this factory.
     def shutdown
       factory.close
+      Pacer.open_graphs.delete url
+    end
+
+    private
+
+    def configure(graph)
+      graph.useLightweightEdges = lightweight_edges if lightweight_edges == false
+      graph.useClassForEdgeLabel = edge_classes if edge_classes == false
+      graph.useClassForVertexLabel = vertex_classes if vertex_classes == false
+      #graph.setAutoStartTx false
+      graph
     end
   end
 end
