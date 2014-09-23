b3f93e42f320a247096c11814aae4591c8a850bf
Darrick Wiebe <dw@xnlogic.com>
2014-09-23 17:57:22 -0400

    Bump blueprints version

--- lib/pacer-orient/version.rb
+++ lib/pacer-orient/version.rb
@@ -4,7 +4,7 @@ module Pacer
     JAR = "pacer-orient-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
     ORIENT_VERSION = "2.0-M2-SNAPSHOT"
-    PIPES_VERSION = "2.5.0"
+    PIPES_VERSION = "2.6.0"
     PACER_REQ = ">= 1.5.3"
   end
 end
