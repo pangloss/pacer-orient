56e3cf88684c63ef3e45bd62ab4ab212f83eb964
Darrick Wiebe <dw@xnlogic.com>
2014-10-16 16:46:31 -0400

    Bump to M2 release and remove orient-commons dependency

--- lib/pacer-orient/version.rb
+++ lib/pacer-orient/version.rb
@@ -1,9 +1,9 @@
 module Pacer
   module Orient
-    VERSION = "2.2.1.pre"
+    VERSION = "2.2.2.pre"
     JAR = "pacer-orient-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    ORIENT_VERSION = "2.0-M2-SNAPSHOT"
+    ORIENT_VERSION = "2.0-M2"
     PIPES_VERSION = "2.6.0"
     PACER_REQ = ">= 1.5.4"
   end
